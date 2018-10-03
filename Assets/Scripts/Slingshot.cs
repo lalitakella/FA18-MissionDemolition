9454f7f889718f60f4e14d668d59be039917a3d2
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-10-03 09:55:02 -0400

    gotta make more castles

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     [Header("Set in Inspetor")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -13,7 +14,17 @@ public class Slingshot : MonoBehaviour {
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake() {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchpoint = launchPointTrans.gameObject;
         launchpoint.SetActive(false);
