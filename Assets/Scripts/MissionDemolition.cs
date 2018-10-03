69ce72086709198de84a024dc09546737a67a2c6
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-10-03 10:49:03 -0400

    pg 538

---
+++ Assets/Scripts/MissionDemolition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+    idle, playing, levelEnd
+}
+public class MissionDemolition : MonoBehaviour {
+    static private MissionDemolition S;
+
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+    public Vector3 castlePos;
+
+}
